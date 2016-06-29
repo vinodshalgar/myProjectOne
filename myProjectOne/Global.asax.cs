2f0d3375d9484da5e561038c2d8ea0a4e2e35dbb
Vinod <vinod.shalgar@gmail.com>
2016-06-29 19:32:58 +0530

    Update Global.asax.cs
    my funny test

--- myProjectOne/Global.asax.cs
+++ myProjectOne/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +13,7 @@ namespace myProjectOne
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //this is just a test
         }
     }
 }
